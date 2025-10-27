c1536cc3ee7fbc987d3aa05d42d312d7f9973ea9
unknown <xuhuazhen0428@gmail.com>
2025-10-27 15:34:48 -0700

    reove .env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainLayout from "./components/mainLayout/mainLayout";
+import { AuthGuardForSignup } from './router/AuthGuard';
+import SignupPage from './pages/Signup';
+import LoginPage from './pages/Login';
 
 const App = () => {
-  return <MainLayout />;
-};
+  // return <MainLayout />;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path='/signup/*'
+          element={
+            <AuthGuardForSignup>
+              <SignupPage />
+            </AuthGuardForSignup>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <LoginPage />
+          }
+        />
+      </Routes>
+    </Router>
+)};
 
 export default App;
---
+++ client/src/components/AuthForm/authForm.jsx
@@ -0,0 +1,83 @@
+import { Form, Input, Button, Card, Switch } from 'antd';
+import {
+    CloseOutlined,
+    MailTwoTone
+} from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+import styles from './style.module.css';
+
+export const AuthForm = ({
+    title,
+    subtitle,
+    fields=[],
+    buttonText,
+    loading,
+    onSubmit,  
+    }) => {
+    
+        const [form] = Form.useForm();
+        const navigate = useNavigate();
+
+        return (
+            <div className={styles.authPage}>
+                <Card className={styles.card} title={
+                    <div className={styles.cardHeader}>
+                        <Button type="text"
+                            className={styles.cardHeaderButton}  
+                            icon={<CloseOutlined />}
+                            onClick={() => navigate('/')}
+                        />        
+                        <span className={styles.cardHeaderTitle}>{title}</span> 
+                        {/* update password subtitle */}
+                        { subtitle && 
+                            <span style={{fontSize: 14, fontWeight: 'normal'}}> 
+                                {subtitle}
+                            </span>
+                        }
+                    </div>
+                } 
+                >
+                  { fields && fields.length > 0 && (
+                    <Form 
+                        form={form} 
+                        layout='vertical'
+                        onFinish={onSubmit}
+                        autoComplete='off'
+                    >
+                        { fields.map((field) => {
+                            return (
+                                <Form.Item className={field.name === 'email' ? styles.readonlyInput : ''}
+                                    key={field.name}
+                                    name={field.name}
+                                    label={field.label}
+                                    rules={field.rules}
+                                    initialValue={field.value || undefined} 
+                                >
+                                    { field.type === 'password' ?
+                                        <Input.Password placeholder={field.placeholder} /> 
+                                        :
+                                        <Input 
+                                            placeholder={field.placeholder} 
+                                            readOnly={field.name === 'email'}
+                                        />
+                                    }
+                                </Form.Item>
+                            );
+                        })} 
+
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                loading={loading}
+                                block
+                            >
+                               {buttonText}
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                    )}
+                </Card>
+            </div>
+        );
+}
